ae5add6c6973d8b2daebda9a3b1e5b0c4aed3fd9
Thomas Cheng <112975095+tcheng529@users.noreply.github.com>
2024-10-17 14:48:16 -0400

    Update _layout.jsx

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,13 +1,20 @@
 import { StyleSheet, Text, View } from 'react-native'
-import { Stack } from 'expo-router'
-import React from 'react'
+import { Stack, useRouter } from 'expo-router';
+import React, { useEffect } from 'react';
 
 const RootLayout = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/login');
+  }, []);
+
   return (
     <Stack>
-      <Stack.Screen name='index' options={{ headerShown: false }} />
+      <Stack.Screen name="login" options={{ headerShown: false }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
     </Stack>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
